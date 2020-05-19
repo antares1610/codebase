4b9c1c8735613dc25c06983bce346062a529b574
Antares1610 <harishariyanto285@yahoo.com>
2020-05-19 16:39:57 +0700

    Perbaikan App\View

--- publishes/app/View/Components/File.php
+++ publishes/app/View/Components/File.php
@@ -9,18 +9,20 @@ class File extends Component
 	public $id;
 	public $name;
 	public $invalid;
-	public $message;
+    public $message;
+    public $label;
 
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct($id = '', $name = '', $message = '')
+    public function __construct($id = '', $name = '', $message = '', $label = '')
     {
         $this->id = $id;
         $this->name = $name;
         $this->message = $message;
+        $this->label = $label;
 
         if (empty($message)) {
         	$this->invalid = false;
