03f791f51d3a2216eea8a59256ce3d8883f909f7
Antares1610 <harishariyanto285@yahoo.com>
2020-05-19 16:27:42 +0700

    Perbaikan Komponen File

--- publishes/views/components/file.blade.php
+++ publishes/views/components/file.blade.php
@@ -1,6 +1,9 @@
 <div class="form-group">
+	@if (!empty($label))
+		<div class="mb-2">{{ $label }}</div>
+	@endif
 	<div class="custom-file">
-		<input type="file" class="custom-file-input {{ $invalid ? 'is-invalid' : '' }}" id="{{  $id }}" name="{{  $name }}">
+		<input type="file" class="custom-file-input {{ $invalid ? 'is-invalid' : '' }}" id="{{ $id }}" name="{{ $name }}">
 		<label class="custom-file-label" for="{{  $id }}">Pilih file</label>
 		@if ($invalid)
 			<span class="invalid-feedback">{{ $message }}</span>
